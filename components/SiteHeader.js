8d0608d272c7cb1702bb8a61b24596c9b6821b36
mbuguap <petrmbugua@gmail.com>
2022-08-10 14:27:10 +0300

    crud

---
+++ components/SiteHeader.js
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import AuthBtn from './AuthBtn';
+
+const SiteHeader = () => {
+  return (
+    <header className={'site-header'}>
+      <div className='wrapper'>
+        <figure className='logo'>
+          <Link href={`/`} passHref={true}>
+            📝 Notes
+          </Link>
+        </figure>
+
+        <AuthBtn />
+      </div>
+    </header>
+  );
+};
+
+export default SiteHeader;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { NoteProvider } from "../modules/AppContext";
+
+import DefaultLayout from "../layouts/default";
+
+import "../styles/globals.css";
+import "../styles/Editor.css";
+import "../styles/SiteHeader.css";
+import "../styles/NoteList.css";
+import "../styles/AuthBtn.css"
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <SessionProvider session={session}>
+      <NoteProvider>
+        <DefaultLayout>
+          <Component {...pageProps} />
+        </DefaultLayout>
+      </NoteProvider>
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
